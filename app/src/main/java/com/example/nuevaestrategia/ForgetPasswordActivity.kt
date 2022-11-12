4d6c486eb65db5f023b3820fc0f98cd3dd4c6d84
margaritamfs <margaritamfs.forero@gmail.com>
2022-11-12 16:30:19 -0500

    12 de noviembre 4:30

--- app/src/main/java/com/example/nuevaestrategia/ForgetPasswordActivity.kt
+++ app/src/main/java/com/example/nuevaestrategia/ForgetPasswordActivity.kt
@@ -10,31 +10,32 @@ import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 
 class ForgetPasswordActivity : AppCompatActivity() {
-    private var editEmailforget: EditText?= null
+    private var editEmailforget: EditText? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_forget_password)
-        editEmailforget= findViewById(R.id.editEmailForget)
+        editEmailforget = findViewById(R.id.editEmailForget)
     }
 
-    fun ToForget(view: View){
-        FirebaseAuth.getInstance().sendPasswordResetEmail(editEmailforget.text.toString())
-            .addOnSuccessListener {
-                if(it.isSuccessful){
+    fun ToForget(view: View) {
+        FirebaseAuth.getInstance().sendPasswordResetEmail(editEmailforget!!.text.toString())
+            .addOnCompleteListener() {
+                if (it.isSuccessful) {
                     Toast.makeText(this, "Correo enviado", Toast.LENGTH_LONG).show()
-                    val intent= Intent(this, MainActivity::class.java)
+                    val intent = Intent(this, MainActivity::class.java)
                     startActivity(intent)
                     finish()
-                }
-            }else{
-                Toast.makeText(this, "Correo no valido", Toast.LENGTH_LONG).show()
-        }
-    }
-}
 
-overrride fun onBackPressed(){
-    super.onBackPressed()
-    val intent= Intent(this, MainActivity::class.java)
-    startActivity(intent)
-    finish()
+                } else {
+                    Toast.makeText(this, "Correo no valido", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
 }
