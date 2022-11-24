3fa0c8e94291bbeb296d20da5df64e94f960a0c8
margaritamfs <margaritamfs.forero@gmail.com>
2022-11-23 19:31:09 -0500

    actualizacion de imagenes 2

--- app/src/main/java/com/example/nuevaestrategia/NuevoProductoActivity.kt
+++ app/src/main/java/com/example/nuevaestrategia/NuevoProductoActivity.kt
@@ -1,5 +1,6 @@
 package com.example.nuevaestrategia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.nuevaestrategia.room_database.AdminProducto.Producto
@@ -14,23 +15,55 @@ class NuevoProductoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_nuevo_producto)
+        var idProducto: Int=0
+        if(intent.hasExtra("producto")){
+            val producto=intent.extras?.getSerializable("producto")as Producto
+            editTextNombreNPA.setText(producto.nombre)
+            editTextPrecioNPA.setText(producto.precio.toString())
+            editTextDetalleNPA.setText(producto.descripcion)
+            idProducto= producto.idProducto
+        }
+
         val database = ProductoDatabase.getDatabase(this)
         val dbFirebase = FirebaseFirestore.getInstance()
         buttonNPA.setOnClickListener{
             val nombre = editTextNombreNPA.text.toString()
             val precio = editTextPrecioNPA.text.toString().toFloat()
             val descripcion = editTextDetalleNPA.text.toString()
-            val producto = Producto(0, nombre, precio, descripcion, R.drawable.ic_launcher_background)
-            CoroutineScope(Dispatchers.IO).launch {
-               var result=database.productos().insert(producto)
+            val producto = Producto(idProducto, nombre, precio, descripcion, R.drawable.ic_launcher_background)
 
-                dbFirebase.collection("Productos").document(result.toString())
-                    .set(hashMapOf("nombre" to nombre, "precio" to precio, "descripcion" to descripcion)
-                    )
+          if (idProducto==0) {
 
+              CoroutineScope(Dispatchers.IO).launch {
+                  var result = database.productos().insert(producto)
 
-                this@NuevoProductoActivity.finish()
-            }
+                  dbFirebase.collection("Productos").document(result.toString())
+                      .set(
+                          hashMapOf(
+                              "nombre" to nombre,
+                              "precio" to precio,
+                              "descripcion" to descripcion
+                          )
+                      )
+                  this@NuevoProductoActivity.finish()
+              }
+          }else {
+              CoroutineScope(Dispatchers.IO).launch {
+                  database.productos().update(producto)
+
+                  dbFirebase.collection("Productos").document(idProducto.toString())
+                      .set(
+                          hashMapOf(
+                              "nombre" to nombre,
+                              "precio" to precio,
+                              "descripcion" to descripcion
+                          )
+                      )
+                  this@NuevoProductoActivity.finish()
+              }
+              val principal=Intent(this,ListaProductoActivity::class.java)
+              startActivity(principal)
+          }
         }
     }
 }
--- app/src/main/java/com/example/nuevaestrategia/ProductoActivity.kt
+++ app/src/main/java/com/example/nuevaestrategia/ProductoActivity.kt
@@ -1,11 +1,17 @@
 package com.example.nuevaestrategia
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import com.example.nuevaestrategia.room_database.AdminProducto.Producto
+import com.example.nuevaestrategia.room_database.AdminProducto.ProductoDatabase
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_producto.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ProductoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,9 +30,24 @@ class ProductoActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
+       val producto=intent.getSerializableExtra("producto")as Producto
+       val database= ProductoDatabase.getDatabase(this)
+       val dbFirebase= FirebaseFirestore.getInstance()
+
         when(item.itemId){
-            R.id.edit_item>{}
-            R.id.delete_item{}
+            R.id.edit_item->{
+                val intent= Intent(this, NuevoProductoActivity::class.java)
+                intent.putExtra("producto", producto)
+                startActivity(intent)
+            }
+            R.id.delete_item->{
+                CoroutineScope(Dispatchers.IO).launch {
+                    database.productos().delete(producto)
+                    dbFirebase.collection("Productos")
+                        .document(producto.idProducto.toString()).delete()
+                    this@ProductoActivity.finish()
+                }
+            }
         }
         return super.onOptionsItemSelected(item)
     }
